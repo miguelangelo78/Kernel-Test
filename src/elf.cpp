73df38e0429884c7a945503d05700076933cda58
Miguel <santosmiguel25@gmail.com>
2016-03-24 23:59:35 +0000

    elf's byte offset bug fixed

--- src/elf.cpp
+++ src/elf.cpp
@@ -20,17 +20,17 @@ static inline elf32_shdr * elf_section(elf32_ehdr * elf_header, char section_idx
 }
 
 inline char * elf_str_table(elf32_ehdr * elf_header) {
-	return elf_header->e_shstrndx == SHN_UNDEF ? 0 : (char*)elf_header + elf_section(elf_header, elf_header->e_shstrndx)->sh_offset;
+	return elf_header->e_shstrndx == SHN_UNDEF ? 0 : (char*)BASE_OFF(elf_header) + elf_section(elf_header, elf_header->e_shstrndx)->sh_offset;
 }
 
-static inline char *elf_lookup_string(elf32_ehdr *elf_header, elf32_shdr * elf_section) {
+static inline char * elf_lookup_string(elf32_ehdr *elf_header, elf32_shdr * elf_section) {
 	char *strtab = elf_str_table(elf_header);
-	return strtab ? strtab + elf_section->sh_name + 1 : 0;
+	return strtab ? strtab + elf_section->sh_name : 0;
 }
 
-static inline char *elf_lookup_string(elf32_ehdr *elf_header, elf32_sym * elf_symtable) {
+static inline char * elf_lookup_string(elf32_ehdr *elf_header, elf32_sym * elf_symtable) {
 	char *strtab = elf_str_table(elf_header);
-	return strtab ? strtab + elf_symtable->st_name + 1 : 0;
+	return strtab ? strtab + elf_symtable->st_name : 0;
 }
 
 static inline elf32_shdr * elf_symsection(elf32_ehdr * header) {
@@ -42,7 +42,7 @@ static inline elf32_shdr * elf_symsection(elf32_ehdr * header) {
 
 static inline elf32_sym * elf_symtable(elf32_ehdr * header) {
 	elf32_shdr * symsection = elf_symsection(header);
-	return symsection ? (elf32_sym*)((uint32_t)header + symsection->sh_offset + 1) : 0;
+	return symsection ? (elf32_sym*)((uint32_t)header + symsection->sh_offset) : 0;
 }
 
 static inline elf32_sym * elf_sym(elf32_ehdr * header, int symidx) {
@@ -58,15 +58,15 @@ static inline uintptr_t elf_get_symval(elf32_ehdr * header, elf32_shdr * symsect
 	if(index >= symtab_entries) return 0;
 
 	elf32_sym * symbol = elf_sym(header, index);
-	return (BASE_OFF(header) + symbol->st_value + elf_section(header, symbol->st_shndx)->sh_offset + 1);
+	return (BASE_OFF(header) + symbol->st_value + elf_section(header, symbol->st_shndx)->sh_offset);
 }
 
-static inline uintptr_t elf_get_symval(elf32_ehdr * header, elf32_shdr * symsection, char * symname, char delimiter) {
+static inline uintptr_t elf_get_symval(elf32_ehdr * header, elf32_shdr * symsection, char * symname, char charlimit) {
 	for(int i = 0; i < elf_symtab_entry_count(symsection);i++) {
 		elf32_sym * symbol = elf_sym(header, i);
 		char * symname_temp = elf_lookup_string(header, symbol);
-		if(delimiter) {
-			char * delim = strchr(symname_temp, (int)delimiter);
+		if(charlimit) {
+			char * delim = strchr(symname_temp, (int)charlimit);
 			if(delim) {
 				int delimindex = (int)((uint32_t)delim - (uint32_t)symname_temp);
 				if(!delimindex++) continue;
@@ -76,13 +76,13 @@ static inline uintptr_t elf_get_symval(elf32_ehdr * header, elf32_shdr * symsect
 
 				if(!strcmp(symbuff, symname)) {
 					free(symbuff);
-					return (BASE_OFF(header) + symbol->st_value + elf_section(header, symbol->st_shndx)->sh_offset + 1);
+					return (BASE_OFF(header) + symbol->st_value + elf_section(header, symbol->st_shndx)->sh_offset);
 				}
 				free(symbuff);
 			}
 		} else {
 			if(!strcmp(symname, symname_temp))
-				return (BASE_OFF(header) + symbol->st_value + elf_section(header, symbol->st_shndx)->sh_offset + 1);
+				return (BASE_OFF(header) + symbol->st_value + elf_section(header, symbol->st_shndx)->sh_offset);
 		}
 	}
 	return 0;
@@ -97,9 +97,7 @@ char * elf_parse(uint8_t * blob, int blobsize) {
 	elf32_shdr * symsection = elf_symsection(head);
 	elf32_sym * symtable = elf_symtable(head);
 
-	Module::modent_t * mod = (Module::modent_t *)elf_get_symval(head, symsection, "modent_", '_');
-	//mod->init();
-	kprintf("symbol: %s", mod->name);
+	kprintf("|%s|", elf_get_symval(head, symsection, "modent_", '_'));
 
 	kprintf("\nDone");
 
