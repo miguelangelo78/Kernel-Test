a78664fc7b376daa92f4c194219b3fee0b41c09e
Miguel <santosmiguel25@gmail.com>
2016-03-28 20:51:28 +0100

    module callback protection added

--- src/module.cpp
+++ src/module.cpp
@@ -8,15 +8,28 @@
 #include <system.h>
 #include <elf.h>
 #include <module.h>
-#include <libc/list.h>
+#include <libc/hashmap.h>
 
 namespace Module {
 
 list_t * modlist;
+hashmap_t * mod_quitlist;
+hashmap_t * mod_initlist;
 int modcount = 0;
 
 #define GET_LMOD(n) ((modent_t*)(n->value))
 #define MOD_IDX_INVALID(idx) ((idx) < 0 || (idx) > list_size(modlist))
+
+/* Once the drivers are initialized, their callbacks will be "erased", for security resons */
+int module_init_cback(void) {
+	return 1;
+}
+
+/* This function will be called everytime anyone tried to call the finish cback of every module.
+ * For security reasons, the function pointers will always lead to this function instead */
+int module_remover_manager(void) {
+	return 1;
+}
 
 uintptr_t module_ioctl(char * modname, void * data) {
 	modent_t * mod = module_get(modname);
@@ -41,7 +54,8 @@ EXPORT_SYMBOL(module_ioctl_i);
 
 modent_t * module_get(char * modname) {
 	foreach(n, modlist)
-		if(!strcmp(GET_LMOD(n)->name, modname)) return (modent_t*)n->value;
+		if(!strcmp(GET_LMOD(n)->name, modname))
+			return (modent_t*)n->value;
 	return 0;
 }
 
@@ -87,17 +101,33 @@ char module_exists(modent_t * mod) {
 	return list_find(modlist, mod) ? 1 : 0;
 }
 
-char module_add(modent_t * mod) {
+int module_add(modent_t * mod) {
 	if(module_exists(mod)) return 0;
+	hashmap_set(mod_initlist, &mod->name[0], (void*)mod->init);
+	hashmap_set(mod_quitlist, &mod->name[0], (void*)mod->finit);
+
+	int ret = mod->init();
+	/* Redirect callbacks: */
+	mod->init = module_init_cback;
+	mod->finit = module_remover_manager;
 	list_insert(modlist, mod);
 	modcount++;
-	return 1;
+	return ret;
 }
 
 char module_remove(char * modname) {
 	int i = 0;
 	foreach(n, modlist) {
-		if(!strcmp(GET_LMOD(n)->name, modname)) {
+		modent_t * mod = GET_LMOD(n);
+		if(!strcmp(mod->name, modname)) {
+			uintptr_t init_addr =  (uintptr_t)hashmap_get(mod_initlist, modname);
+			uintptr_t finit_addr = (uintptr_t)hashmap_get(mod_quitlist, modname);
+			FCASTF(finit_addr, int, void)(); /* Call finit */
+			/* Restore callbacks (only necessary because the modules are not copies, they point to the actual modules): */
+			mod->init = FCASTF(init_addr, int, void);
+			mod->finit = FCASTF(finit_addr, int, void);
+			hashmap_remove(mod_initlist, modname);
+			hashmap_remove(mod_quitlist, modname);
 			list_remove(modlist, i);
 			modcount--;
 			return 1;
@@ -128,6 +158,8 @@ char module_remove(modent_t * mod) {
 void modules_load(void) {
 	int modcount = initrd_modcount();
 	modlist = list_create();
+	mod_initlist = hashmap_create(modcount);
+	mod_quitlist = hashmap_create(modcount);
 
 	kprintf("\n - Total modules: %d", modcount);
 
@@ -145,18 +177,16 @@ void modules_load(void) {
 			modent_t * modentry = (modent_t *)elf_find_mod((elf32_ehdr*)modblob);
 			if(modentry == (modent_t*)MOD_UNKNOWN) {
 				kprintf(" | (UNKNOWN module)");
-			}
-			else {
+			} else {
 				kprintf("%s", !modentry->init || modentry == (modent_t *)MOD_DEP ? " | (Dependable object)":" | Initializing...", strchr(modentry->name, MODULE_SIGNATURE1)+1);
 			}
 
 			if(modentry && modentry != (modent_t *)MOD_DEP && modentry->init) {
-				kprintf(" > ret: %d", modentry->init());
-				module_add(modentry);
-			}
-			else {
+				kprintf(" > ret: %d", module_add(modentry));
+			} else {
+				/* We don't run modules that don't have an entry point */
 				continue;
-			} /* We don't run modules that don't have an entry point */
+			}
 		} else {
 			kprintf(" | ERROR: Failed loading ELF");
 		}
