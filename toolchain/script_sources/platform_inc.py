a1c27f6258bd81dfb6a6e371991f5d22850232c2
Miguel <santosmiguel25@gmail.com>
2016-06-27 22:06:20 -0400

    Platform include script bug fixed for Debian systems

--- toolchain/script_sources/platform_inc.py
+++ toolchain/script_sources/platform_inc.py
@@ -5,4 +5,6 @@ def plat_get():
 	if plat == "Linux":
 		# Which Linux is this?
 		plat = commands.getstatusoutput("cat /etc/os-release | grep 'ID_LIKE.*'")[1][8:].capitalize()
+		if not plat:
+			plat = commands.getstatusoutput("cat /etc/os-release | grep 'ID=[A-Za-z]'")[1][3:].capitalize()
 	return plat
