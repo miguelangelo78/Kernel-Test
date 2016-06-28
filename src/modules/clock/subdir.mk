680824dd474d753f155e652714ba5200cfe60b7a
Miguel <santosmiguel25@gmail.com>
2016-06-28 19:20:30 +0100

    Launcher script changed back to original executable path

--- src/modules/clock/subdir.mk
+++ src/modules/clock/subdir.mk
@@ -1,18 +1,18 @@
 OBJS +=
 MODS += \
-$(BOUT)/modules/pit.mod \
-$(BOUT)/modules/cmos.mod
-
-$(BOUT)/modules/pit.mod: src/modules/clock/pit.cpp 
-	@echo '>> Building file $<'
-	@echo '>> Invoking LLVM C++ Clang++'
-	$(CXX_LLVM) $(LLVMCPPFLAGS) -r -fno-zero-initialized-in-bss -O2 -W -Wall -Wstrict-prototypes -Wmissing-prototypes -D__KERNEL__ -DMODULE -o obj/modules/pit.mod -c $<  
-	@echo '>> Finished building: $<'
-	@echo ' '
+$(BOUT)/modules/cmos.mod \
+$(BOUT)/modules/pit.mod
 
 $(BOUT)/modules/cmos.mod: src/modules/clock/cmos.cpp 
 	@echo '>> Building file $<'
 	@echo '>> Invoking LLVM C++ Clang++'
 	$(CXX_LLVM) $(LLVMCPPFLAGS) -r -fno-zero-initialized-in-bss -O2 -W -Wall -Wstrict-prototypes -Wmissing-prototypes -D__KERNEL__ -DMODULE -o obj/modules/cmos.mod -c $<  
+	@echo '>> Finished building: $<'
+	@echo ' '
+
+$(BOUT)/modules/pit.mod: src/modules/clock/pit.cpp 
+	@echo '>> Building file $<'
+	@echo '>> Invoking LLVM C++ Clang++'
+	$(CXX_LLVM) $(LLVMCPPFLAGS) -r -fno-zero-initialized-in-bss -O2 -W -Wall -Wstrict-prototypes -Wmissing-prototypes -D__KERNEL__ -DMODULE -o obj/modules/pit.mod -c $<  
 	@echo '>> Finished building: $<'
 	@echo ' '
--- src/subdir.mk
+++ src/subdir.mk
@@ -1,25 +1,18 @@
 OBJS += \
-$(BOUT)/module.o \
-$(BOUT)/error.o \
-$(BOUT)/elf.o \
-$(BOUT)/video.o \
 $(BOUT)/args.o \
-$(BOUT)/log.o \
-$(BOUT)/shm.o \
-$(BOUT)/serial.o \
-$(BOUT)/vfs.o \
-$(BOUT)/kmain.o \
+$(BOUT)/elf.o \
+$(BOUT)/error.o \
 $(BOUT)/initrd.o \
-$(BOUT)/syscall.o
+$(BOUT)/kmain.o \
+$(BOUT)/log.o \
+$(BOUT)/module.o \
+$(BOUT)/serial.o \
+$(BOUT)/shm.o \
+$(BOUT)/syscall.o \
+$(BOUT)/vfs.o \
+$(BOUT)/video.o
 
-$(BOUT)/module.o: src/module.cpp 
-	@echo '>> Building file $<'
-	@echo '>> Invoking LLVM C++ Clang++'
-	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
-	@echo '>> Finished building: $<'
-	@echo ' '
-
-$(BOUT)/error.o: src/error.cpp 
+$(BOUT)/args.o: src/args.cpp 
 	@echo '>> Building file $<'
 	@echo '>> Invoking LLVM C++ Clang++'
 	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
@@ -33,49 +26,7 @@ $(BOUT)/elf.o: src/elf.cpp
 	@echo '>> Finished building: $<'
 	@echo ' '
 
-$(BOUT)/video.o: src/video.cpp 
-	@echo '>> Building file $<'
-	@echo '>> Invoking LLVM C++ Clang++'
-	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
-	@echo '>> Finished building: $<'
-	@echo ' '
-
-$(BOUT)/args.o: src/args.cpp 
-	@echo '>> Building file $<'
-	@echo '>> Invoking LLVM C++ Clang++'
-	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
-	@echo '>> Finished building: $<'
-	@echo ' '
-
-$(BOUT)/log.o: src/log.cpp 
-	@echo '>> Building file $<'
-	@echo '>> Invoking LLVM C++ Clang++'
-	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
-	@echo '>> Finished building: $<'
-	@echo ' '
-
-$(BOUT)/shm.o: src/shm.cpp 
-	@echo '>> Building file $<'
-	@echo '>> Invoking LLVM C++ Clang++'
-	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
-	@echo '>> Finished building: $<'
-	@echo ' '
-
-$(BOUT)/serial.o: src/serial.cpp 
-	@echo '>> Building file $<'
-	@echo '>> Invoking LLVM C++ Clang++'
-	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
-	@echo '>> Finished building: $<'
-	@echo ' '
-
-$(BOUT)/vfs.o: src/vfs.cpp 
-	@echo '>> Building file $<'
-	@echo '>> Invoking LLVM C++ Clang++'
-	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
-	@echo '>> Finished building: $<'
-	@echo ' '
-
-$(BOUT)/kmain.o: src/kmain.cpp 
+$(BOUT)/error.o: src/error.cpp 
 	@echo '>> Building file $<'
 	@echo '>> Invoking LLVM C++ Clang++'
 	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
@@ -89,7 +40,56 @@ $(BOUT)/initrd.o: src/initrd.cpp
 	@echo '>> Finished building: $<'
 	@echo ' '
 
+$(BOUT)/kmain.o: src/kmain.cpp 
+	@echo '>> Building file $<'
+	@echo '>> Invoking LLVM C++ Clang++'
+	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
+	@echo '>> Finished building: $<'
+	@echo ' '
+
+$(BOUT)/log.o: src/log.cpp 
+	@echo '>> Building file $<'
+	@echo '>> Invoking LLVM C++ Clang++'
+	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
+	@echo '>> Finished building: $<'
+	@echo ' '
+
+$(BOUT)/module.o: src/module.cpp 
+	@echo '>> Building file $<'
+	@echo '>> Invoking LLVM C++ Clang++'
+	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
+	@echo '>> Finished building: $<'
+	@echo ' '
+
+$(BOUT)/serial.o: src/serial.cpp 
+	@echo '>> Building file $<'
+	@echo '>> Invoking LLVM C++ Clang++'
+	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
+	@echo '>> Finished building: $<'
+	@echo ' '
+
+$(BOUT)/shm.o: src/shm.cpp 
+	@echo '>> Building file $<'
+	@echo '>> Invoking LLVM C++ Clang++'
+	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
+	@echo '>> Finished building: $<'
+	@echo ' '
+
 $(BOUT)/syscall.o: src/syscall.cpp 
+	@echo '>> Building file $<'
+	@echo '>> Invoking LLVM C++ Clang++'
+	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
+	@echo '>> Finished building: $<'
+	@echo ' '
+
+$(BOUT)/vfs.o: src/vfs.cpp 
+	@echo '>> Building file $<'
+	@echo '>> Invoking LLVM C++ Clang++'
+	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
+	@echo '>> Finished building: $<'
+	@echo ' '
+
+$(BOUT)/video.o: src/video.cpp 
 	@echo '>> Building file $<'
 	@echo '>> Invoking LLVM C++ Clang++'
 	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
