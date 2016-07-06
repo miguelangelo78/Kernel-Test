f40fced4e173e7ff0ca23a3f5c7274e59ecad1a8
miguelangelo78 <santosmiguel25@gmail.com>
2016-07-07 00:20:53 +0100

    hda.img added to Debian's launcher scripts

--- src/libc/subdir.mk
+++ src/libc/subdir.mk
@@ -1,7 +1,7 @@
 OBJS += \
 $(BOUT)/libcc.o \
-$(BOUT)/kprintf.o \
-$(BOUT)/libc.o
+$(BOUT)/libc.o \
+$(BOUT)/kprintf.o
 
 $(BOUT)/libcc.o: src/libc/libcc.c 
 	@echo '>> Building file $<'
@@ -10,14 +10,14 @@ $(BOUT)/libcc.o: src/libc/libcc.c
 	@echo '>> Finished building: $<'
 	@echo ' '
 
-$(BOUT)/kprintf.o: src/libc/kprintf.cpp 
+$(BOUT)/libc.o: src/libc/libc.cpp 
 	@echo '>> Building file $<'
 	@echo '>> Invoking LLVM C++ Clang++'
 	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
 	@echo '>> Finished building: $<'
 	@echo ' '
 
-$(BOUT)/libc.o: src/libc/libc.cpp 
+$(BOUT)/kprintf.o: src/libc/kprintf.cpp 
 	@echo '>> Building file $<'
 	@echo '>> Invoking LLVM C++ Clang++'
 	$(CXX_LLVM) $(LLVMCPPFLAGS)  -o $@ -c $<  
--- toolchain/Debian/launch_x86_64.sh
+++ toolchain/Debian/launch_x86_64.sh
@@ -10,7 +10,7 @@ x-terminal-emulator -e gdb -x toolchain/gdb.gdbinit
 # Launch one of the following available Virtual Machines:
 #*********** AVAILABLE VIRTUAL MACHINES: ***********
 
-qemu-system-x86_64 -cdrom iso/KernelSharp.iso -serial stdio -gdb tcp:127.0.0.1:1234 -m 128
+qemu-system-x86_64 -cdrom iso/KernelSharp.iso -hda iso/hda.img -serial stdio -gdb tcp:127.0.0.1:1234 -m 128
 #virtualbox --startvm KernelSharp --dbg
 #vmplayer toolchain/VMachs/VMWare/Linux/KernelSharp/KernelSharp.vmx
 
