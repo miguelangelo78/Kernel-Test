e0d8281434c0b0c6978ea39c4633668af63d6f4a
Miguel <santosmiguel25@gmail.com>
2016-07-11 16:14:57 +0100

    EXT2: Implementation (block/inode readers/writers) functions added

--- src/modules/fs/ext2.cpp
+++ src/modules/fs/ext2.cpp
@@ -18,6 +18,15 @@
 #define SB (fs->superblock)
 #define DC (fs->disk_cache)
 
+/*
+ * These macros deal with the block group descriptor bitmap
+ */
+#define BLOCKBIT(n)  (bg_buffer[((n) >> 3)] & NTH_BIT((((n) % 8))))
+#define BLOCKBYTE(n) (bg_buffer[((n) >> 3)])
+#define SETBIT(n)    NTH_BIT((((n) % 8)))
+
+#define GETFS(node) (ext2_fs_t*)node->device
+
 enum E_CODES {
 	E_SUCCESS,
 	E_BADBLOCK,
@@ -58,24 +67,64 @@ typedef struct ext2_fs {
  ****** FUNCTION PROTOTYPES ******
  *********************************/
 static int read_block(ext2_fs_t * fs, unsigned int block_no, uint8_t * buff);
+static int write_block(ext2_fs_t * fs, unsigned int block_no, uint8_t *buff);
+static ext2_inodetable_t * read_inode(ext2_fs_t * fs, uint32_t inode);
+static int write_inode(ext2_fs_t * fs, ext2_inodetable_t * inode, uint32_t index);
+static unsigned int ext2_sync(ext2_fs_t * fs);
+static int cache_flush_dirty(ext2_fs_t * fs, unsigned int ent_no);
+static ext2_dir_t * ext2_direntry(ext2_fs_t * fs, ext2_inodetable_t * inode, uint32_t no, uint32_t index);
+static void refresh_inode(ext2_fs_t * fs, ext2_inodetable_t * inodet, uint32_t inode);
+static unsigned int inode_read_block(ext2_fs_t * fs, ext2_inodetable_t * inode, unsigned int block, uint8_t * buff);
+static unsigned int inode_write_block(ext2_fs_t * fs, ext2_inodetable_t * inode, unsigned int inode_no, unsigned int block, uint8_t * buff);
+static unsigned int allocate_block(ext2_fs_t * fs);
+static unsigned int set_block_number(ext2_fs_t * fs, ext2_inodetable_t * inode, unsigned int inode_no, unsigned int iblock, unsigned int rblock);
+
 
 /*********************************************/
 /***** EXT2 FILESYSTEM HANDLER FUNCTIONS *****/
 /*********************************************/
 static uint32_t ext2_open(FILE * node, unsigned int flags) {
+	ext2_fs_t * fs = GETFS(node);
+	if(flags & O_TRUNC) {
+		ext2_inodetable_t * inode = read_inode(fs, node->inode);
+		inode->size = 0;
+		write_inode(fs, inode, node->inode);
+	}
 	return 0;
 }
 
 static uint32_t ext2_close(FILE * node) {
+	/* Nothing to do here */
 	return 0;
 }
 
 static int ext2_chmod(FILE * node, int mode) {
+	ext2_fs_t * fs = GETFS(node);
+	ext2_inodetable_t * inode = read_inode(fs, node->inode);
+	inode->mode = (inode->mode & 0xFFFFF000) | mode;
+	write_inode(fs, inode, node->inode);
+	ext2_sync(fs);
 	return 0;
 }
 
 static struct dirent * ext2_readdir(FILE * node, uint32_t index) {
-	return 0;
+	ext2_fs_t * fs = GETFS(node);
+	ext2_inodetable_t * inode = read_inode(fs, node->inode);
+	/* Fetch directory entry: */
+	ext2_dir_t * direntry = ext2_direntry(fs, inode, node->inode, index);
+	if(!direntry) {
+		free(inode);
+		return 0;
+	}
+
+	/* Cast direntry into dirent struct: */
+	struct dirent * dirent = (struct dirent*)malloc(sizeof(struct dirent));
+	memcpy(&dirent->name, &direntry->name, direntry->name_len);
+	dirent->name[direntry->name_len] = '\0';
+	dirent->ino = direntry->inode;
+	free(direntry);
+	free(inode);
+	return dirent;
 }
 
 static FILE * ext2_finddir(FILE * node, char * name) {
@@ -83,20 +132,122 @@ static FILE * ext2_finddir(FILE * node, char * name) {
 }
 
 static void ext2_create(FILE * parent, char * name, uint16_t permission) {
-
+	// TODO
 }
 
 static void ext2_mkdir(FILE * parent, char * name, uint16_t permission) {
-
+	// TODO
 }
 
 static void ext2_unlink(FILE * node, char * name) {
-
+	// TODO
 }
 
 /*****************************************/
 /***** EXT2 IMPLEMENTATION FUNCTIONS *****/
 /*****************************************/
+
+/**
+ * ext2->rewrite_superblock Rewrite the superblock.
+ *
+ * Superblocks are a bit different from other blocks, as they are always in the same place,
+ * regardless of what the filesystem block size is. This doesn't work well with our setup,
+ * so we need to special-case it.
+ */
+static int rewrite_superblock(ext2_fs_t * fs) {
+	fwrite(fs->block_device, 1024, sizeof(ext2_superblock_t), (uint8_t *)SB);
+	return E_SUCCESS;
+}
+
+/**
+ * ext2->allocate_inode_block Allocate a block in an inode.
+ *
+ * @param inode Inode to operate on
+ * @param inode_no Number of the inode (this is not part of the struct)
+ * @param block Block within inode to allocate
+ * @returns Error code or E_SUCCESS
+ */
+static int allocate_inode_block(ext2_fs_t * fs, ext2_inodetable_t * inode, unsigned int inode_no, unsigned int block) {
+	unsigned int block_no = allocate_block(fs);
+	if(!block_no) return E_NOSPACE;
+
+	set_block_number(fs, inode, inode_no, block, block_no);
+	unsigned int t = (block + 1) * (fs->block_size / 512);
+	if(inode->blocks < t) {
+		inode->blocks = t;
+	}
+	write_inode(fs, inode, inode_no);
+	return E_SUCCESS;
+}
+
+/**
+ * ext2->get_block_number Given an inode block number, get the real block number.
+ *
+ * @param inode   Inode to operate on
+ * @param iblock  Block offset within the inode
+ * @returns Real block number
+ */
+static unsigned int get_block_number(ext2_fs_t * fs, ext2_inodetable_t * inode, unsigned int iblock) {
+	// TODO
+}
+
+/**
+ * ext2->set_block_number Set the "real" block number for a given "inode" block number.
+ *
+ * @param inode   Inode to operate on
+ * @param iblock  Block offset within the inode
+ * @param rblock  Real block number
+ * @returns Error code or E_SUCCESS
+ */
+static unsigned int set_block_number(ext2_fs_t * fs, ext2_inodetable_t * inode, unsigned int inode_no, unsigned int iblock, unsigned int rblock) {
+	// TODO
+}
+
+
+static ext2_dir_t * ext2_direntry(ext2_fs_t * fs, ext2_inodetable_t * inode, uint32_t no, uint32_t index) {
+	uint8_t * block = (uint8_t*)malloc(fs->block_size);
+	uint8_t block_nr = 0;
+	inode_read_block(fs, inode, block_nr, block);
+
+	uint32_t dir_offset = 0;
+	uint32_t total_offset = 0;
+	uint32_t dir_index = 0;
+
+	while (total_offset < inode->size && dir_index <= index) {
+		ext2_dir_t *d_ent = (ext2_dir_t *)((uintptr_t)block + dir_offset);
+		if(d_ent->inode != 0 && dir_index == index) {
+			ext2_dir_t *out = (ext2_dir_t*)malloc(d_ent->rec_len);
+			memcpy(out, d_ent, d_ent->rec_len);
+			free(block);
+			return out;
+		}
+
+		dir_offset += d_ent->rec_len;
+		total_offset += d_ent->rec_len;
+
+		if(d_ent->inode)
+			dir_index++;
+
+		if(dir_offset >= fs->block_size) {
+			block_nr++;
+			dir_offset -= fs->block_size;
+			inode_read_block(fs, inode, block_nr, block);
+		}
+	}
+
+	free(block);
+	return 0;
+}
+
+static unsigned int ext2_sync(ext2_fs_t * fs) {
+	spin_lock(fs->lock);
+	/* Flush each cache entry: */
+	for(unsigned int i = 0; i < fs->cache_entries; i++)
+		if(DC[i].dirty)
+			cache_flush_dirty(fs, i);
+	spin_unlock(fs->lock);
+	return 0;
+}
 
 /****** CACHE FUNCTIONS ******/
 /**
@@ -121,6 +272,89 @@ static int cache_flush_dirty(ext2_fs_t * fs, unsigned int ent_no) {
 }
 
 /****** BLOCK / INODE READERS / WRITERS ******/
+
+static unsigned int allocate_block(ext2_fs_t * fs) {
+	unsigned int block_no = 0;
+	unsigned int block_offset = 0;
+	unsigned int group = 0;
+	uint8_t * bg_buffer = (uint8_t*)malloc(fs->block_size);
+
+	for(unsigned int i = 0; i < fs->block_group_count; i++) {
+		if(fs->block_groups[i].free_blocks_count > 0) {
+			read_block(fs, fs->block_groups[i].block_bitmap, (uint8_t*)bg_buffer);
+			while(BLOCKBIT(block_offset))
+				block_offset++;
+			block_no = block_offset + SB->blocks_per_group * i;
+			group = i;
+			break;
+		}
+	}
+
+	if(!block_no) {
+		kprintf("\n\t> ERROR: No available blocks. Disk is out of space!");
+		free(bg_buffer);
+		return 0;
+	}
+
+	BLOCKBYTE(block_offset) |= SETBIT(block_offset);
+	write_block(fs, fs->block_groups[group].block_bitmap, (uint8_t*)bg_buffer);
+
+	fs->block_groups[group].free_blocks_count--;
+	for(int i = 0; i <fs->bgd_block_span; i++)
+		write_block(fs, fs->bgd_offset + i, (uint8_t *)((uint32_t)fs->block_groups + fs->block_size * i));
+
+	SB->free_blocks_count--;
+	rewrite_superblock(fs);
+
+	memset(bg_buffer, 0, fs->block_size);
+	write_block(fs, block_no, bg_buffer);
+
+	free(bg_buffer);
+	return block_no;
+}
+
+/**
+ * ext2->inode_read_block
+ *
+ * @param inode
+ * @param no
+ * @param block
+ * @parma buf
+ * @returns Real block number for reference.
+ */
+static unsigned int inode_read_block(ext2_fs_t * fs, ext2_inodetable_t * inode, unsigned int block, uint8_t * buff) {
+	if(block >= inode->blocks / (fs->block_size / 512)) {
+		memset(buff, 0, fs->block_size);
+		kprintf("\n\tERROR: Tried to read an invalid block. Asked for %d but inode only has %d!", block, inode->blocks / (fs->block_size / 512));
+		return 0;
+	}
+	unsigned int real_block = get_block_number(fs, inode, block);
+	read_block(fs, real_block, buff);
+	return real_block;
+}
+
+/**
+ * ext2->inode_write_block
+ */
+static unsigned int inode_write_block(ext2_fs_t * fs, ext2_inodetable_t * inode, unsigned int inode_no, unsigned int block, uint8_t * buff) {
+	if(block >= inode->blocks / (fs->block_size / 512)) {
+		kprintf("\n\tERROR: Attempting to write beyond the existing allocated blocks for this inode");
+		kprintf(" (Inode %d, Block %d)", inode_no, block);
+	}
+
+	char * empty = 0;
+	while(block >= inode->blocks / (fs->block_size / 512)) {
+		allocate_inode_block(fs, inode, inode_no, inode->blocks / (fs->block_size / 512));
+		refresh_inode(fs, inode, inode_no);
+	}
+
+	if(empty) free(empty);
+
+	unsigned int real_block = get_block_number(fs, inode, block);
+	write_block(fs, real_block, buff);
+	return real_block;
+}
+
 static void refresh_inode(ext2_fs_t * fs, ext2_inodetable_t * inodet, uint32_t inode) {
 	uint32_t group = inode / fs->inodes_per_group;
 	if(group > fs->block_group_count)
@@ -146,6 +380,81 @@ static ext2_inodetable_t * read_inode(ext2_fs_t * fs, uint32_t inode) {
 	ext2_inodetable_t * inodet = (ext2_inodetable_t*)malloc(fs->inode_size);
 	refresh_inode(fs, inodet, inode);
 	return inodet;
+}
+
+static int write_inode(ext2_fs_t * fs, ext2_inodetable_t * inode, uint32_t index) {
+	uint32_t group = index / fs->inodes_per_group;
+	if(group > fs->block_group_count)
+		return E_BADBLOCK;
+
+	uint32_t inode_table_block = fs->block_groups[group].inode_table;
+	index -= group * fs->inodes_per_group;
+	uint32_t block_offset = ((index - 1) * fs->inode_size) / fs->block_size;
+	uint32_t offset_in_block = (index - 1) - block_offset * (fs->block_size / fs->inode_size);
+
+	ext2_inodetable_t * inodet = (ext2_inodetable_t*)malloc(fs->block_size);
+	/* Read the current table block: */
+	read_block(fs, inode_table_block + block_offset, (uint8_t*)inodet);
+	memcpy((uint8_t *)((uint32_t)inodet + offset_in_block * fs->inode_size), inode, fs->inode_size);
+	write_block(fs, inode_table_block + block_offset, (uint8_t*)inodet);
+	free(inodet);
+	return E_SUCCESS;
+}
+
+/**
+ * ext2->write_block Write a block to the block device.
+ *
+ * @param block_no Block to write
+ * @param buf      Data in the block
+ * @returns Error code or E_SUCCESSS
+ */
+static int write_block(ext2_fs_t * fs, unsigned int block_no, uint8_t *buff) {
+	if(block_no == 0) {
+		kprintf("\n\t> ERROR: Attempted to write to block #0. Aborting");
+		return E_BADBLOCK;
+	}
+
+	spin_lock(fs->lock);
+	if(!DC) {
+		/* Write to block device (such as ATA): */
+		fwrite(fs->block_device, block_no * fs->block_size, fs->block_size, buff);
+		spin_unlock(fs->lock);
+		return E_SUCCESS;
+	}
+
+	/* Else: Find the entry in the cache: */
+	int oldest = -1;
+	unsigned int oldest_age = UINT32_MAX;
+	for(unsigned int i = 0; i <fs->cache_entries; i++) {
+		if(DC[i].block_no == block_no) {
+			/* We found it. Update the cache entry */
+			DC[i].last_use = get_cache_time(fs);
+			DC[i].dirty = 1;
+			memcpy(DC[i].block, buff, fs->block_size);
+			spin_unlock(fs->lock);
+			return E_SUCCESS;
+		}
+		if(DC[i].last_use < oldest_age) {
+			/* Keep track of the oldest entry */
+			oldest = i;
+			oldest_age = DC[i].last_use;
+		}
+	}
+
+	/* We did not find this element in the cache, so make room */
+	if(DC[oldest].dirty)
+		/* Flush the oldest entry */
+		cache_flush_dirty(fs, oldest);
+
+	/* Update the entry: */
+	memcpy(DC[oldest].block, buff, fs->block_size);
+	DC[oldest].block_no = block_no;
+	DC[oldest].last_use = get_cache_time(fs);
+	DC[oldest].dirty = 1;
+
+	/* Done: */
+	spin_unlock(fs->lock);
+	return E_SUCCESS;
 }
 
 /**
--- src/version/version.cpp
+++ src/version/version.cpp
@@ -14,7 +14,7 @@ char * ver_kernel_version_fmt = (char*)"%d.%d.%d";
 
 int ver_kernel_major = 0;
 int ver_kernel_minor = 0;
-int ver_kernel_lower = 2355;
+int ver_kernel_lower = 2366;
 
 char * ver_kernel_codename = VER_UNKNOWN;
 
