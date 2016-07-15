263eb95b0f5a2fd750e01f32d6c48eed8a636801
Miguel <santosmiguel25@gmail.com>
2016-07-15 05:34:38 +0100

    Bootable usb script for Windows fixed

--- toolchain/Windows/bootable_usb.bat
+++ toolchain/Windows/bootable_usb.bat
@@ -6,26 +6,24 @@
 @if "%drive%" == "c" ( @printf "ERROR: Do not use an invalid drive (like C:)\n" && pause && exit )
 @if "%drive%" == "C" ( @printf "ERROR: Do not use an invalid drive (like C:)\n" && pause && exit )
 
-@cd ..\..\iso\Windows\syslinux
+@cd ..\..\iso\boot\syslinux
 
-copy isolinux.bin %drive%:\
+copy isolinux\isolinux.bin %drive%:\
 copy libcom32.c32 %drive%:\
 copy mboot.c32 %drive%:\
 copy syslinux.cfg %drive%:\
-copy syslinux.c32 %drive%:\
 copy ldlinux.c32 %drive%:\
 copy libutil.c32 %drive%:\
 copy vesamenu.c32 %drive%:\
-copy background.png %drive%:\
+copy bg.png %drive%:\
 
 @cd ..\..
 
 copy initrd.img %drive%:\
 copy ksharp.bin %drive%:\
 
-cd Windows\syslinux
+cd boot\syslinux
 syslinux.exe --mbr --active --directory / --install %drive%:
-
 
 @printf "\nCongratulations. The USB %drive% is now bootable. Restart your computer to boot it.\n"
 
