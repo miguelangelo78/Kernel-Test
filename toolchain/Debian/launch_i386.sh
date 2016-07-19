ea0737ec0712f5eeff943786c28e9c78451d12c2
Miguel <santosmiguel25@gmail.com>
2016-07-19 01:53:10 +0100

    More fix

--- toolchain/Debian/launch_i386.sh
+++ toolchain/Debian/launch_i386.sh
@@ -3,8 +3,15 @@ cd `dirname $0`
 source ./sheader.sh
 clear
 
-printf "\nLaunching Kernel... (x86_64)\n"
+printf "\nLaunching Kernel... (i386)\n"
 # Launch GDB in a new terminal:
 x-terminal-emulator -e gdb -x toolchain/gdb.gdbinit
-# Open QEMU:
-qemu-system-i386 -cdrom iso/KernelSharp.iso -serial stdio -gdb tcp:127.0.0.1:1234 -m 128
+
+# Launch one of the following available Virtual Machines:
+#*********** AVAILABLE VIRTUAL MACHINES: ***********
+
+qemu-system-i386 -cdrom iso/KernelSharp.iso -hda iso/hda.img -serial stdio -gdb tcp:127.0.0.1:1234 -m 256
+#virtualbox --startvm KernelSharp --dbg
+#vmplayer toolchain/VMachs/VMWare/Linux/KernelSharp/KernelSharp.vmx
+
+#****************************************************
